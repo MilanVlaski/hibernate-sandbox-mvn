fcf5472d55a51ba1bdcce9cf68e41ab9ad551ed7
Milan Vlaski <milan.vlaski321@gmail.com>
2024-09-28 08:28:54 +0200

    initial commit

---
+++ src/main/java/com/akimi/hibernate_sandbox_mvn/HibernateSandboxMvnApplication.java
@@ -0,0 +1,14 @@
+package com.akimi.hibernate_sandbox_mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSandboxMvnApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(HibernateSandboxMvnApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/akimi/hibernate_sandbox_mvn/HibernateSandboxMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akimi.hibernate_sandbox_mvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSandboxMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
